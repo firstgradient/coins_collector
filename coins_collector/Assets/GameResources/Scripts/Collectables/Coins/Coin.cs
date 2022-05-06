c237ad0a277a603764b72b5bd5efdff116f23ccc
firstgradient <firstgradient@gmail.com>
2022-05-06 20:00:20 +0600

    Prepare for build

--- coins_collector/Assets/GameResources/Scripts/Collectables/Coins/Coin.cs
+++ coins_collector/Assets/GameResources/Scripts/Collectables/Coins/Coin.cs
@@ -9,9 +9,20 @@ public class Coin : BaseCollectable
     [SerializeField]
     private int _timeBonus = 0;
 
+    [SerializeField]
+    private GameObject _collectedEffect;
+
     protected override void Collected()
     {
+        BurstEffect();
+
         BasicEventManager.PublishEvent(CoinsEvents.COIN_COLLECTED, new CoinsEvents.CoinCollectedEventArgs(_score));
         BasicEventManager.PublishEvent(TimerEvents.ADD_REMAINING_TIME, new TimerEvents.AddRemainingTimeEventArgs(_timeBonus));
+    }
+
+    private void BurstEffect()
+    {
+        var newEffect = Object.Instantiate(_collectedEffect, transform.position, Quaternion.Euler(-90,0,0));
+        Object.Destroy(newEffect, newEffect.GetComponent<ParticleSystem>().main.duration);
     }
 }
